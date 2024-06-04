f89395a91f855939bc77bfab39ca4d8e31f04100
Pop <shounen.pop@gmail.com>
2024-06-04 14:25:03 +0700

    add: button and alert-box

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,34 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+export function Alert(props) {
+  return (
+    <div
+      css={css`
+        margin: 10px; 0px;
+        display: flex;
+        width: 300px;
+        padding: 5px 20px;
+        gap: 10px;
+        font-weight: 800;
+        border-radius: 15px;
+        color: grey;
+        background-color: ${
+          props.type === "error"
+            ? "#F9C8C8"
+            : props.type === "warning"
+            ? "#F9D9C8"
+            : props.type === "info"
+            ? "#F9EBC8"
+            : props.type === "success"
+            ? "#CEF7CD"
+            : "white"
+        };
+      `}
+    >
+      <p>😘</p>
+      <p>This is {props.type} alert box</p>
+    </div>
+  );
+}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,56 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  return (
+    <div
+      css={css`
+        display: flex;
+        width: 100%;
+        align-items: center;
+        flex-direction: row;
+        justify-content: space-between;
+      `}
+    >
+      <p>{props.type}</p>
+      {props.type === "primary" ? (
+        <button
+          css={css`
+            padding: 10px 30px;
+            background-color: #074ee8;
+          `}
+        >
+          Button
+        </button>
+      ) : props.type === "secondary" ? (
+        <button
+          css={css`
+            padding: 10px 30px;
+            background-color: #07a4e8;
+          `}
+        >
+          Button
+        </button>
+      ) : props.type === "error" ? (
+        <button
+          css={css`
+            padding: 10px 30px;
+            background-color: #de5753;
+          `}
+        >
+          Button
+        </button>
+      ) : props.type === "success" ? (
+        <button
+          css={css`
+            padding: 10px 30px;
+            background-color: #26b795;
+          `}
+        >
+          Button
+        </button>
+      ) : null}
+    </div>
+  );
+}
