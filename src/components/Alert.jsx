169d45dd5db3bd6f709bdb74af78125b82631bee
Kan <kanalovefootball@gmail.com>
2024-06-05 15:38:48 +0700

    add: Button and Alert box

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,52 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import ErrorIcon from "../assets/Icon/frown";
+import WarningIcon from "../assets/Icon/alert-triangle";
+import InfoIcon from "../assets/Icon/alert-circle";
+import SuccessIcon from "../assets/Icon/check-circle";
 
-function Alert() {}
+function Alert(props) {
+  let bgColor = "#F9EBC8";
+  let AlertIcon = InfoIcon;
+
+  if (props.severity === "error") {
+    bgColor = "#F9C8C8";
+    AlertIcon = ErrorIcon;
+  } else if (props.severity === "warning") {
+    bgColor = "#F9D9C8";
+    AlertIcon = WarningIcon;
+  } else if (props.severity === "sucess") {
+    bgColor = "#CEF7CD";
+    AlertIcon = SuccessIcon;
+  }
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: ${bgColor};
+      `}
+    >
+      <div
+        css={css`
+          margin-right: 20px;
+        `}
+      >
+        <AlertIcon />
+      </div>
+      {props.message}
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,33 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  let bgColor = undefined;
+  if (props.color === "primary") {
+    bgColor = "#074EE8";
+  } else if (props.color === "secondary") {
+    bgColor = "#07A4E8";
+  }
+
+  return (
+    <button
+      css={css`
+        display: block;
+        background-color: ${bgColor};
+        margin: 10px;
+        width: 170px;
+        height: 50px;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        font-weight: 400;
+        font-size: 16px;
+        font-family: "Sarabun", sans-serif;
+      `}
+    >
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
